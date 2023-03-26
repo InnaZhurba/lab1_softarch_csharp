99005942ec3bccb26650942699212edda9ec94c1
innazhurba <inazhurba@gmail.com>
2023-03-26 11:33:37 +0300

    added facadeService

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -1,12 +1,15 @@
 using System.Net;
 using Facade.Controllers;
+using Facade.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+FacadeService facadeService = new FacadeService(builder.Services.BuildServiceProvider().GetService<ILoggerFactory>());
+
 // facade service send requests to logging and messages services
 // it get requests from client service like POST/GET
-var facadeController = new FacadeController( builder.Services.BuildServiceProvider().GetService<ILoggerFactory>());
+var facadeController = new FacadeController( builder.Services.BuildServiceProvider().GetService<ILoggerFactory>(), facadeService);
 
 //send post request to save message to loggingservice from http request body
 app.MapPost("/post", async (HttpRequest request) =>
---
+++ Facade/Services/FacadeService.cs
@@ -0,0 +1,72 @@
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Facade.Services;
+
+public class FacadeService
+{
+    private readonly ILogger _logger;
+    
+    // created WebClient`s for logging and messages services
+    private WebClient loggingService = new WebClient();
+    private WebClient messagesService = new WebClient();
+    
+    // URIs of logging and messages services
+    //string loggingURI = "http://localhost:5248/";
+    private string messagesURI = "http://localhost:5133/";
+    
+    // list of loggingURLs with URLS : "http://localhost:5248/", "http://localhost:5247/", http://localhost:5249/"
+    private static string[] loggingURLs = new string[] {"http://localhost:5248/", "http://localhost:5247/", "http://localhost:5249/"};
+    
+    //choose random loggingURL from loggingURLs
+    private string loggingURI = loggingURLs[new Random().Next(0, loggingURLs.Length)];
+
+    
+    public FacadeService(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<FacadeService>();
+    }
+    
+    // GET request
+    public string GetMessages()
+    {
+        //message: get messages from MessagesController
+        _logger.LogInformation("Get messages from MessagesController");
+        var messages =  messagesService.DownloadString(messagesURI+"get");
+        var logging = loggingService.DownloadString(loggingURI+"get");
+        
+        /*
+        logging = logging.TrimStart('\"');
+        logging = logging.TrimEnd('\"');
+        logging = logging.Replace("\\", "");
+        */
+
+        //convert json to string format
+        // using nuget package Newtonsoft.Json for convert json to string format
+        messages = JsonConvert.DeserializeObject<string>(messages);
+
+        //convert json to List<string> format
+        // using nuget package Newtonsoft.Json for convert json to List<string> format
+        var new_logging = JsonConvert.DeserializeObject<string>(logging);
+
+        return new_logging + " : " +  messages;
+    }
+    
+    // POST request
+    public void AddMessage(string message)
+    {
+        //message: added to LoggingController
+        _logger.LogInformation($"Message added to LoggingController: {message}");
+
+        //convert string to json format
+        var json = new StringContent(message, Encoding.UTF8, "application/json");
+
+        //send message to logging service in json format 
+        // logging service is the other web microservice in other project
+        loggingService.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+        
+        // using nuget package Newtonsoft.Json for convert string to json format 
+        loggingService.UploadString(loggingURI + "post", JsonConvert.ToString(message));
+    }
+}
